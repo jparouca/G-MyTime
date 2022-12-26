fde3987402678b08755bc23dd018b14e9b036b7a
jparouca <joaopearoucaa@gmail.com>
2022-12-25 17:14:18 -0800

    better error handling

---
+++ src/pages/api/users/index.api.ts
@@ -0,0 +1,27 @@
+import type { NextApiRequest, NextApiResponse } from 'next'
+import {prisma} from '../../../lib/prisma'
+
+export default async function handler(req: NextApiRequest, res: NextApiResponse) {
+  if (req.method != 'POST') {
+    return res.status(405).end()
+  }
+  const { name, username } = req.body
+  const userExists = await prisma.user.findUnique({
+    where: {
+      username,
+    }
+  })
+  if (userExists) {
+    return res.status(409).json({message: 'Username already exists'})
+  }
+
+
+  const user = await prisma.user.create({
+    data: {
+      name,
+      username,
+    },
+  })
+
+  return res.status(201).json(user)
+}
