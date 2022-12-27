507dbcc725addf51db801bce4fcf3b774d358e96
jparouca <joaopearoucaa@gmail.com>
2022-12-27 00:47:57 -0800

    prisma db adapter: updateUser method

--- src/lib/auth/prisma-adapter.ts
+++ src/lib/auth/prisma-adapter.ts
@@ -75,11 +75,80 @@ export function PrismaAdapter(): Adapter {
     },
 
     // async deleteUser(userId) {},
-    // async linkAccount(account) {},
+
+    async linkAccount(account) {
+      await prisma.account.create({
+        data: {
+          user_id: account.userId,
+          type: account.type,
+          provider: account.provider,
+          provider_account_id: account.providerAccountId,
+          refresh_token: account.refresh_token,
+          access_token: account.access_token,
+          expires_at: account.expires_at,
+          token_type: account.token_type,
+          scope: account.scope,
+          id_token: account.id_token,
+          session_state: account.session_state,
+        },
+      })
+    },
+
     // async unlinkAccount({ providerAccountId, provider }) {},
-    // async createSession({ sessionToken, userId, expires }) {},
-    // async getSessionAndUser(sessionToken) {},
-    // async updateSession({ sessionToken }) {},
+
+    async createSession({ sessionToken, userId, expires }) {
+      await prisma.session.create({
+        data: {
+          session_token: sessionToken,
+          user_id: userId,
+          expires: expires!,
+        },
+      })
+      return {
+        sessionToken,
+        userId,
+        expires,
+      }
+    },
+
+    async getSessionAndUser(sessionToken) {
+      const { user, ...session } = await prisma.session.findUniqueOrThrow({
+        where: { session_token: sessionToken },
+        include: {
+          user: true,
+        },
+      })
+      return {
+        session: {
+          userId: session.user_id,
+          expires: session.expires,
+          sessionToken: session.session_token,
+        },
+        user: {
+          id: user.id,
+          name: user.name,
+          username: user.username,
+          email: user.email!,
+          emailVerified: null,
+          avatar_url: user.avatar_url!,
+        },
+      }
+    },
+    async updateSession({ sessionToken, userId, expires }) {
+      const prismaSession = await prisma.session.update({
+        where: { session_token: sessionToken },
+        data: {
+          session_token: sessionToken,
+          user_id: userId,
+          expires: new Date(),
+        },
+      })
+      return {
+        sessionToken: prismaSession.session_token,
+        userId: prismaSession.user_id,
+        expires: prismaSession.expires,
+      }
+    },
     // async deleteSession(sessionToken) {},
     // async createVerificationToken({ identifier, expires, token }) {},
     // async useVerificationToken({ identifier, token }) {},
