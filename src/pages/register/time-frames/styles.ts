db079e3f3a4d39e7359d9b092c36df8d4dda95b6
jparouca <joaopearoucaa@gmail.com>
2023-01-02 22:54:27 -0300

    register page: init->  avaiability hours

---
+++ src/pages/register/time-frames/styles.ts
@@ -0,0 +1,48 @@
+import { Box, styled } from "@ignite-ui/react"
+
+export const FrameBox = styled(Box, {
+  marginTop: '$6',
+  display: 'flex',
+  flexDirection: 'column',
+  
+  button: {
+    backgroundImage: 'linear-gradient(90deg, #FF008E 0%, #FFCD1E 100%)',
+    border: 'white',
+    color: 'white',
+  },
+})
+
+
+export const FrameContainer = styled('div', {
+  border: '1px solid white',
+  borderRadius: '3px',
+  marginBottom: '$4',
+})
+
+export const FrameItem = styled('h3', {
+  display : 'flex',
+  alignItems: 'center',
+
+  justifyContent: 'space-between',
+  padding: '$3 $4',
+
+  '& + &': {
+    borderTop: '1px solid white',
+  },
+})
+
+export const FrameDay = styled('div', {
+  display: 'flex',
+  alignItems: 'center',
+  gap: '$12',
+})
+
+export const FrameInputs = styled('div', {
+  display: 'flex',
+  alignItems: 'center',
+  gap: '$2',
+
+  'input::-webkit-calendar-picker-indicator': {
+    filter: 'invert(1) brightness(0.7)',
+  }
+})
